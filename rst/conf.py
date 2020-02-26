61614a78d3e7ab2fe2999c328e101d9fef3dee51
onishi <nnnkjktkt119@gmail.com>
2020-02-26 16:09:51 +0900

    test html

--- rst/conf.py
+++ rst/conf.py
@@ -13,13 +13,13 @@
 import os
 import sys
 sys.path.insert(0, '/mnt/local/home/onishi/Git/pygtool3/pygtool3')
-
+import sphinx_rtd_theme
 
 # -- Project information -----------------------------------------------------
 
 project = 'pygtool3'
-copyright = '2020, Author'
-author = 'Author'
+copyright = '2020, Onishi'
+author = 'Onishi'
 
 
 # -- General configuration ---------------------------------------------------
@@ -54,7 +54,8 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinxdoc'
+html_theme = 'sphinx_rtd_theme'
+html_theme_path=[sphinx_rtd_theme.get_html_theme_path()]
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
--- rst/index.rst
+++ rst/index.rst
@@ -1,5 +1,5 @@
 .. pygtool3 documentation master file, created by
-   sphinx-quickstart on Wed Feb 26 15:12:08 2020.
+   sphinx-quickstart on Wed Feb 26 15:59:06 2020.
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
